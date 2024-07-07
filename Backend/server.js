27b084bd2c5f2ceea817e585f6a960fd55573c0f
FourSage747 <129331961+FourSage747@users.noreply.github.com>
2024-07-07 15:07:43 +0200

    test

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,8 @@
 const app = require("./app");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.DB_PORT;
 
 app.listen(PORT, () => {
+  console.log(process.env.DB_NAME);
   console.log(`Server is running on port ${PORT}`);
 });

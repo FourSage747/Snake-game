b5f6db86e3c81939488d6c75630fe62e0ce70d76
FourSage747 <129331961+FourSage747@users.noreply.github.com>
2024-07-07 16:16:19 +0200

    test

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "your_username",
-  host: "localhost",
-  database: "snakegame",
-  password: "your_password",
+  user: "foursage",
+  host: "dpg-cq5a3ho8fa8c7383vkjg-a",
+  database: "snakegame_qvvj",
+  password: "8ovBYaSqaM5PMAWrON5rWKkElkp5wzPP",
   port: 5432,
 });
 
